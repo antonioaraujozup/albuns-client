de9a247fbd1bb098cd486afdf8a95ea63732e250
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-22 18:22:01 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/albunsclient/AlbunsClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.albunsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbunsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbunsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/albunsclient/AlbunsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.albunsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbunsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
